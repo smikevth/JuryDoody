96ef4b133fa856fbfb65af2127d1704f51e171b8
Kevin Smith <smikevth@gmail.com>
2024-04-13 12:25:55 -0700

    basic game loop done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,24 +14,34 @@ public class GameManager : MonoBehaviour
 
     //the two buttons are set for now, but I'd like to instantiate them as prefabs later for the option of more buttons and zaniness.
     [SerializeField]
+    GameObject answerButtons;
+    [SerializeField]
     TMP_Text answerText1;
     [SerializeField]
     TMP_Text answerText2;
+    [SerializeField]
+    GameObject restartButton;
 
     private QAndA currentQuestion;
 
     // Start is called before the first frame update
     void Start()
     {
-        ResetData();
-        //make 1st Q and A
-        MakeQuestion();
+        InitializeGame();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InitializeGame()
+    {
+        ResetData();
+        ToggleRestartButton(false);
+        //make 1st Q and A
+        MakeQuestion();
     }
 
     private void ResetData()
@@ -46,39 +56,68 @@ public class GameManager : MonoBehaviour
 
     private void MakeQuestion()
     {
-        //select question from gameData list using questionsIndex. Will just go in order for now
-        currentQuestion = gameData.questions[gameData.questionsIndexes[0]];
-        //set q and a texts
-        if(currentQuestion.question != null)
+        //check if any questions are left
+        if(gameData.questionsIndexes.Count > 0)
         {
-            questionText.text = currentQuestion.question;
+            //select question from gameData list using questionsIndex. Will just go in order for now
+            currentQuestion = gameData.questions[gameData.questionsIndexes[0]];
+            //null checks
+            bool errors = false;
+            if (currentQuestion.question == null)
+            {
+                Debug.Log("missing question text at index " + gameData.questionsIndexes[0]);
+                errors = true;
+            }
+            if (currentQuestion.answers[0] == null)
+            {
+                Debug.Log("missing answer1 text at index " + gameData.questionsIndexes[0]);
+                errors = true;
+            }
+            if (currentQuestion.answers[1] == null)
+            {
+                Debug.Log("missing answer1 text at index " + gameData.questionsIndexes[1]);
+                errors = true;
+            }
+            if (!errors)
+            {
+                //set q and a texts
+                questionText.text = currentQuestion.question;
+                answerText1.text = currentQuestion.answers[0].answer;
+                answerText2.text = currentQuestion.answers[1].answer;
+            }
+            else
+            {
+                //get a new question maybe
+            }
         }
         else
         {
-            Debug.Log("missing question text at index " + gameData.questionsIndexes[0]);
+            //no more questions, end game
+            if(gameData.selectionScore > gameData.scoreThreshold)
+            {
+                questionText.text = "You have been selected, you lose";
+            }
+            else
+            {
+                questionText.text = "You have been excused, you win";
+            }
+            Debug.Log("final score " + gameData.selectionScore);
+            ToggleRestartButton(true);
         }
-        if(currentQuestion.answers[0] != null)
-        {
-            answerText1.text = currentQuestion.answers[0].answer;
-        }
-        else
-        {
-            Debug.Log("missing answer1 text at index " + gameData.questionsIndexes[0]);
-        }
-        if (currentQuestion.answers[1] != null)
-        {
-            answerText2.text = currentQuestion.answers[1].answer;
-        }
-        else
-        {
-            Debug.Log("missing answer1 text at index " + gameData.questionsIndexes[1]);
-        }
+
+    }
+
+    //when true, the restart button will be enabled and answer buttons will be disabled
+    private void ToggleRestartButton(bool value)
+    {
+        restartButton.SetActive(value);
+        answerButtons.SetActive(!value);
     }
 
     public void ButtonAnswer(int index)
     {
-        //debug to make sure the right thing is happening
-        Debug.Log(currentQuestion.answers[index].answer + " was pressed");
+
+        Debug.Log("current score" + gameData.selectionScore);
         //increment score
         gameData.selectionScore += currentQuestion.answers[index].value;
         //remove question index
